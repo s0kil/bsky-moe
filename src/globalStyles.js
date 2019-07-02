8f633ca3f8ef4f55b1a769691b9761f413cffe45
bsky <git@bsky.moe>
2019-07-02 15:00:56 +0900

    Remove unnecessary font family

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -6,9 +6,8 @@ const globalStyles = css`
   :global() {
     ${normalize()}
     * {
-      font-family: 'Noto Sans JP', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-        Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
-        sans-serif;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+        Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
       -webkit-font-smoothing: antialiased;
       -moz-osx-font-smoothing: grayscale;
     }
